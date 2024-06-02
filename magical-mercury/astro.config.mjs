c191e55388935634e156bbd113710f968e86f8f3
Santiago Perez Delfino <santiago.perez@alumnos.frgp.utn.edu.ar>
2024-06-02 05:32:40 -0300

    -

--- magical-mercury/astro.config.mjs
+++ magical-mercury/astro.config.mjs
@@ -3,4 +3,11 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     base: '/Post-Mors/', // Reemplaza con el nombre de tu repositorio
     // otras configuraciones
+    server: {
+        // Configurar Astro para servir recursos estáticos desde la carpeta 'assets'
+        static: {
+          '/public': './public' // Ruta de acceso a la carpeta de recursos estáticos
+        }
+    }
+    
   });
