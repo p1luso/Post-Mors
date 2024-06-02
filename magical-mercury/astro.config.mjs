c193ba8b84e52db543ffa10a2be53845381b8ffb
Santiago Perez Delfino <santiago.perez@alumnos.frgp.utn.edu.ar>
2024-06-02 05:18:44 -0300

    Update astro.config.mjs

--- magical-mercury/astro.config.mjs
+++ magical-mercury/astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    base: 'Post-Mors', // Reemplaza con el nombre de tu repositorio
+    // otras configuraciones
+  });
