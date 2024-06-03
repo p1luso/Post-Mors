f1322716830dd71e6bef3be36785a01868cc00f7
Santiago Perez Delfino <santiperezdelfino@gmail.com>
2024-06-02 21:35:32 -0300

    .

--- magical-mercury/astro.config.mjs
+++ magical-mercury/astro.config.mjs
@@ -3,25 +3,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     base: '/Post-Mors/', // Reemplaza con el nombre de tu repositorio
     // otras configuraciones
-    middleware: [
-      {
-        name: 'php',
-        async handler(request, response) {
-          if (request.url.endsWith('.php')) {
-            const phpPath = `./pages${request.url}`;
-            return response.send(await runPhp(phpPath, request));
-          }
-          return response.next();
-        },
-      },
-    ],
-  });
 
-  async function runPhp(phpPath, request) {
-    const php = require('php-parser');
-    const phpCode = await fs.readFile(phpPath, 'utf8');
-    const phpOutput = await php.execute(phpCode, {
-      request: request,
-    });
-    return phpOutput;
-  }
+  });
