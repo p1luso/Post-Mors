d9efb6b15f43c6d6f381eac1ea50d12d01f745e3
Santiago Perez Delfino <santiagoperezdelfino@gmail.com>
2024-06-17 21:25:48 -0300

    Update astro.config.mjs

--- magical-mercury/astro.config.mjs
+++ magical-mercury/astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    base: '/Post-Mors/', // Reemplaza con el nombre de tu repositorio
+    base: 'Post-Mors/', // Reemplaza con el nombre de tu repositorio
     // otras configuraciones
 
   });
