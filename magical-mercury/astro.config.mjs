de98211994aee8295eee81e794e7458e4e4ac343
Santiago <santiagoperezdelfino@gmail.com>
2024-06-26 19:49:31 -0300

    changes in config

--- magical-mercury/astro.config.mjs
+++ magical-mercury/astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://p1luso.github.io',
+  site: 'https://p1luso.github.io/Post-Mors/',
   base: '/Post-Mors'
 });
